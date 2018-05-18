35cbfc9230f1288580ac17c42f78e26a5ccf6e1a
Jesper Lekland <jesper.lekland@push2prod.se>
2018-05-18 14:57:13 +0200

    update package json

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ExpandableContent from './expandableContent'
+import Origin from './origin'
+import Target from './target'
+
+export default ExpandableContent;
+
+export {
+  Origin,
+  Target,
+}
